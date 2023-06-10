36a1369c742337d5877dc5693f4b6e08d45d257a
adeshewa12345 <133238028+adeshewa12345@users.noreply.github.com>
2023-06-10 14:29:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+import React from "react"
+const  (userinfo) => state 
+    return{
+        <
+    }
+}
+const app = () =>
+const [firstname, lastname] = usestate ([]);
+const [email address, phone number] = usestate(false)
+ const (userinfo)
+ return {
+    state ={
+        firstname:[
+            {name:"kaffy"};
+            lastname:[
+                {lastname:"tope"};
+                email[
+                    {email:"jimohkafayatomoshalewa"};
+
+                ]
+            ]
+        ]
+    }
+ 
+return(){
+    return(
+        <div app ="app">
+  
+        </div>
+    )
+}
+
+
+import react ,[userstate]
